3586fc11c7fcb1903eb70c6aade5d98dff854e56
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-05-22 12:25:11 -0600

    Add current_price method

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,8 @@
 class Currency < ApplicationRecord
+  def current_price
+    url = 'https://api.coinmarketcap.com/v1/ticker/'
+    request = HTTParty.get(url + self.slug)
+    response = JSON.parse(request.body)
+  end
 end
+
