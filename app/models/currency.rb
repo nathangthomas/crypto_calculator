99d99156bcf96b719e20b57dcea1344038b8d550
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-05-25 23:13:19 -0600

    Add working url

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,8 +1,8 @@
 class Currency < ApplicationRecord
   def current_price
-    url = 'https://api.coinmarketcap.com/v1/ticker/'
+    url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&convert=USD&ENV["CMC_PRO_API_KEY"]"
+
     request = HTTParty.get(url + self.slug)
     response = JSON.parse(request.body)
   end
 end
-
